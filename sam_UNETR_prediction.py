994596f39fc24107bc28708f6293e96a66a4ab49
mahdics68 <113525590+mahdics68@users.noreply.github.com>
2024-01-12 23:22:50 +0100

    Create sam_UNETR_prediction.py

---
+++ sam_UNETR_prediction.py
@@ -0,0 +1,88 @@
+import torch
+import glob
+import torch_em
+from torch_em.model import UNETR
+import imageio.v2 as imageio
+from torch_em.util.prediction import predict_with_halo
+import os
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+
+
+
+
+def test_unetr(args, model_weights: str, cell_type:str, pred_dir:str):
+       
+    cell_types =  ["[MCF7]"] #"[A172]", "[BT474]", "[BV2]", "[Huh7]", "[MCF7]", "[SHSY5Y]", "[SkBr3]", "[SKOV3]"
+    for i in cell_types:
+                
+
+                
+                test_images = os.path.join("/scratch/projects/cca/data/livecell/LiveCELL/images/livecell_test_images/", i+"*")
+
+                model = UNETR(
+                    backbone=args.backbone, encoder=args.encoder, out_channels=2, use_sam_stats=args.use_sam_stats,
+                    final_activation="Sigmoid",
+                    )
+        
+   
+                model.load_state_dict(torch.load(model_weights, map_location=torch.device('cpu'))["model_state"])
+
+                device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+                model.to(device)
+
+                with torch.no_grad():
+                    
+                    for img_path in glob.glob(test_images):
+                        
+                        filename = os.path.split(img_path)[-1] 
+                        # print(i, " -- ", filename)
+
+                        inputs = imageio.imread(img_path)
+                        #print('before std', inputs.shape)
+
+                        #inputs = torch_em.transform.raw.standardize(inputs) ###for pretrained : no need #### for scratch: need it
+                        
+                        predictions = predict_with_halo(inputs, model, gpu_ids=[device], block_shape=(128,128), halo = (48,48))
+                       # for pretrained: preprocess = none , 
+                       # for scratch : remove it (or preprocess=standardize  which is default )
+                        
+
+                        segmentation = predictions[0,:,:]
+                        boundaries = predictions[1,:,:]
+                        
+                        
+                        
+                        
+                        seg_dir = os.path.join("/scratch-grete/usr/nimmahen/models/UNETR/sam/prediction/", pred_dir , "segmentation/")
+                        os.makedirs(seg_dir, exist_ok=True)
+                        imageio.imwrite(os.path.join(seg_dir, filename), segmentation)
+
+                        bd_dir = os.path.join("/scratch-grete/usr/nimmahen/models/UNETR/sam/prediction/", "predictions/", "boundaries/")
+                        os.makedirs(bd_dir, exist_ok=True)
+                        imageio.imwrite(os.path.join(bd_dir, filename), boundaries)
+                    
+                        
+
+def main(args):
+    test_unetr(
+        args,
+        model_weights=args.model_weights, 
+        cell_type=args.cell_type,)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--encoder", default="vit_b")
+    parser.add_argument("--backbone")
+    parser.add_argument("--use_sam_stats", action="store_true")
+    parser.add_argument("--model_weights")
+    parser.add_argument("--cell_type")
+    args = parser.parse_args()
+    
+    main(args)
+                        
+#/scratch/users/menayat/models/UNETR_LiveCell_MAE_SC/checkpoints/unetr-source-livecell-MCF7/best.pt
